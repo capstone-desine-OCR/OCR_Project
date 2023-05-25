9f00fd5e4694725cbb82096ded72466e505e9c25
jookbooin <3670lsh@naver.com>
2023-05-26 07:21:31 +0900

    23_05_26 fragment -> adapter -> dialog -> fragment 수정 ocr도 수정 완료

--- app/src/main/java/com/example/camerakt/ListOcrFragment.kt
+++ app/src/main/java/com/example/camerakt/ListOcrFragment.kt
@@ -44,43 +44,42 @@ class ListOcrFragment : Fragment() {
     // View의 초기값 설정 , LiveData 옵저빙 ,RecyclerView , Adapter 세팅은 이 메소드에서 해주는 것
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val data = arrayListOf(
-            arrayListOf(
-                "번호",
-                "코드",
-                "원산지",
-                "품종",
-                "수입 날짜",
-                "반입 날짜",
-                "중량",
-                "수량",
-                "단가",
-                "총액",
-                "비고"
-            ),
-            arrayListOf("1", "A", "Seoula", "a", "2023-05-01", "2023-05-30a", "akg", "10", "10000", "100000", "None"),
-            arrayListOf("2", "b", "Seoulb", "b", "2023-05-01", "2023-05-30b", "bkg", "10", "10000", "100000", "None"),
-            arrayListOf("3", "c", "Seoulc", "c", "2023-05-01", "2023-05-30c", "ckg", "10", "10000", "100000", "None"),
-            arrayListOf("4", "d", "Seould", "d", "2023-05-01", "2023-05-30d", "dkg", "10", "10000", "100000", "None"),
-            arrayListOf("5", "e", "Seoule", "e", "2023-05-01", "2023-05-30e", "ekg", "10", "10000", "100000", "None"),
-        )
-//        val adapter = TableAdapter(data)
-        val adapter = TableAdapter(data, listViewModel) // 생성자로써 전달
-        binding.recyclerView.adapter = adapter
-        binding.recyclerView.layoutManager =
-            LinearLayoutManager(requireContext()) // context , requiredContext() 동일 -> null 포함여부
-
-//        val adapter
-//      observe
-//        requireContext() : Activity -> fragment 로 주는 context
-//        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext()) // 선형 배치
+//        val data = arrayListOf(
+//            arrayListOf(
+//                "번호",
+//                "코드",
+//                "원산지",
+//                "품종",
+//                "수입 날짜",
+//                "반입 날짜",
+//                "중량",
+//                "수량",
+//                "단가",
+//                "총액",
+//                "비고"
+//            ),
+//            arrayListOf("1", "A", "Seoula", "a", "2023-05-01", "2023-05-30a", "akg", "10", "10000", "100000", "None"),
+//            arrayListOf("2", "b", "Seoulb", "b", "2023-05-01", "2023-05-30b", "bkg", "10", "10000", "100000", "None"),
+//            arrayListOf("3", "c", "Seoulc", "c", "2023-05-01", "2023-05-30c", "ckg", "10", "10000", "100000", "None"),
+//            arrayListOf("4", "d", "Seould", "d", "2023-05-01", "2023-05-30d", "dkg", "10", "10000", "100000", "None"),
+//            arrayListOf("5", "e", "Seoule", "e", "2023-05-01", "2023-05-30e", "ekg", "10", "10000", "100000", "None"),
+//        )
 //
-////         data -> : observe에 의해  ArrayList<ArrayList<String>>의 변경이 감지되었을때 -> 이후 실행 ( data를 adapter로 넘긴다)
-//        listViewModel.listTableData.observe(viewLifecycleOwner) { data ->
-////            val adapter = TableAdapter(data)
-//            val adapter = TableAdapter(data, listViewModel)
-//            binding.recyclerView.adapter = adapter // recyclerView 로 adapter 를 붙여주는 것 -> ViewHolder를 붙여주는 것
-//        }
+//        val adapter = TableAdapter(data, listViewModel) // 생성자로써 전달
+//        binding.recyclerView.adapter = adapter
+//        binding.recyclerView.layoutManager =
+//            LinearLayoutManager(requireContext()) // context , requiredContext() 동일 -> null 포함여부
+
+        var adapter: TableAdapter = TableAdapter(ArrayList(), listViewModel)
+//        requireContext() : Activity -> fragment 로 주는 context
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext()) // 선형 배치
+
+
+//         data -> : observe에 의해  ArrayList<ArrayList<String>>의 변경이 감지되었을때 -> 이후 실행 ( data를 adapter로 넘긴다)
+        listViewModel.listTableData.observe(viewLifecycleOwner) { data ->
+            adapter = TableAdapter(data, listViewModel)
+            binding.recyclerView.adapter = adapter // recyclerView 로 adapter 를 붙여주는 것 -> ViewHolder를 붙여주는 것
+        }
 
         listViewModel.editRowData.observe(viewLifecycleOwner) { modifiedRow ->
             Log.d("edit Row1", "수정가능")
